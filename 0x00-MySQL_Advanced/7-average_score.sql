a9052d17cc2489f546a98755f5e7dd1409a57cba
AliyahTanzil <sesaymohamdaugustin@gmail.com>
2023-07-16 08:51:27 +0000

    changes

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,27 @@
--- This script creates a stored procedure named ComputeAverageScoreForUser that computes and stores the average score for a student.
--- First, we set the delimiter to $$ so that we can use semicolons inside the procedure.
-DELIMITER $$ -- Next, we create the procedure.
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT) BEGIN -- Declare a variable to hold the average score.
-DECLARE avg_score DECIMAL(10, 2);
--- Calculate the average score for the specified user.
-SELECT AVG(score) INTO avg_score
-FROM scores
-WHERE user_id = user_id;
--- Insert the result into the average_scores table.
-INSERT INTO average_scores (user_id, avg_score)
-VALUES (user_id, avg_score);
-END $$ -- Finally, we reset the delimiter back to ;.
-DELIMITER;
+-- SQL script to create a stored procedure to compute and store the average score for a student
+
+-- Description: This stored procedure takes a user_id as input and calculates the average score for the corresponding student.
+
+-- Drop the stored procedure if it already exists
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+-- Create the stored procedure
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+
+    -- Calculate the average score for the given user_id
+    SELECT AVG(score) INTO avg_score
+    FROM scores
+    WHERE user_id = user_id;
+
+    -- Insert or update the average score for the user
+    INSERT INTO average_scores (user_id, average_score)
+    VALUES (user_id, avg_score)
+    ON DUPLICATE KEY UPDATE average_score = avg_score;
+
+    -- Optionally, you can return the average score as output
+    SELECT avg_score AS average_score;
+END $$
+DELIMITER ;
--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
--- Import the table dump
-SOURCE names.sql.zip;
--- Create an index named idx_name_first on the table names and the first letter of name
+-- This script creates an index named idx_name_first on the table names and the first letter of name.
+-- First, we import the table dump.
+SOURCE / path / to / names.sql;
 CREATE INDEX idx_name_first ON names (LEFT(name, 1));
